264e494d17f7106ca324584b887d295c46462c6b
vic778 <victoiremmanuelbarh@gmail.com>
2024-03-27 23:54:13 +0400

    add concurrency feature to fix fix the performance of the database in case of many users books on the same tine an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,23 @@
 class Event < ApplicationRecord
+  lock_optimistically
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  before_save :set_remaining_ticket, if: -> { new_record? }
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, length: { minimum: 10 }
   validates :location, :date_and_time, presence: true
   validates :total_tickets, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def available_for_booking?
+    remaining_ticket.positive?
+  end
+
+  protected
+
+  def set_remaining_ticket
+    self.remaining_ticket = total_tickets
+  end
 end
--- app/services/booking_service.rb
+++ app/services/booking_service.rb
@@ -6,19 +6,32 @@ class BookingService
     @event = event
     @current_user = current_user
     @bookings_params = bookings_params
-    @booking = current_user.bookings.create(bookings_params)
+    @booking = current_user.bookings.build(bookings_params)
   end
 
   def call
     booking.event = event
-    booking.ticket_number = @booking.generate_reference
+    booking.ticket_number = booking.generate_reference
 
-    update_remaining_ticket(@booking, @booking.event) if booking.save
-    booking
+    event.with_lock do
+      if enough_tickets_available?
+        booking.save!
+        update_remaining_ticket(booking, event)
+        booking
+      else
+        booking.errors.add(:base, 'Not enough tickets available')
+      end
+    end
+  end
+
+  private
+
+  def enough_tickets_available?
+    event.remaining_ticket >= booking.ticket_quantity
   end
 
   def update_remaining_ticket(booking, event)
-    event.remaining_ticket -= booking.ticket_quantity
-    event.save
+    remaining_tickets = event.remaining_ticket - booking.ticket_quantity
+    event.update!(remaining_ticket: remaining_tickets)
   end
 end
