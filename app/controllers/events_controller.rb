94deb78207c1d86bc06f2ad0982c5940492c9641
vic778 <victoiremmanuelbarh@gmail.com>
2024-03-27 21:19:33 +0400

    Create meths for crud the event. and before action aunt the user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,50 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  before_action :set_event, only: %i[show edit update destroy]
+
   def index
-     @events = Rails.cache.fetch('events', expires_in: 1.hour) do
+    @events = Rails.cache.fetch('events', expires_in: 1.hour) do
       Event.all.order("name ASC")
     end
+  end
+
+  def show; end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.create(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Event was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_url, notice: 'Event was successfully destroyed.'
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :date_and_time, :total_tickets, :user_id)
   end
 end
