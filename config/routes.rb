e9bf871b4dcc23b12c54c9679c9d89313cbe14ca
vic778 <victoiremmanuelbarh@gmail.com>
2024-03-27 21:47:42 +0400

    Triger current user events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'events#index'
-  devise_for :users 
-    resources :events
+  devise_for :users
+  resources :events
+  get 'my_events', to: 'events#current_user_events'
   
 end
